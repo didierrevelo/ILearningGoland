c34f37e46a2fb7287d22db3535cf9728f2f73967
didierrevelo <didierrevelo@gmail.com>
2022-11-01 09:53:53 -0500

    condicionales if else

---
+++ ejer03/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+var status bool
+func main() {
+		status = true
+		if status=false; status==true {
+			fmt.Println("Status is true")
+		} else {
+			fmt.Println("Status is false")
+		}
+}
